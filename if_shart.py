cbce790e64913c959dd2731fc10e926ab7f23cf6
Ikromjon <ikromjon01021995@gmail.com>
2026-04-25 14:46:22 +0500

    list & tuple

--- if_shart.py
+++ if_shart.py
@@ -1,1 +1,78 @@
 #if shart operatori
+
+# username = 'ikromjon'
+# password = 'salom2026'
+#
+# login = input('Enter your login name: ')
+# temp_password = input('Enter your password: ')
+#
+# if login == username and password == temp_password:
+#     print('Login Successful\n foydalanuvchi sahifasiga xush kelibsiz')
+#
+# else:
+#     print('Login failed')
+#
+#
+#
+#
+# # match case
+# # ball = 5,4,3,2,1,0
+# ball = 10
+# if ball == 5:
+#     print("A'lo")
+# elif ball == 4:
+#     print("Yaxshi")
+# elif ball == 3:
+#     print("Qoniqarli")
+# elif ball == 2:
+#     print("Qoniqarsiz")
+# elif ball == 1:
+#     print("Dabdala")
+# else:
+#     print("Bunday baho bebaho")
+#
+# match ball:
+#     case 5:
+#         print("A'lo")
+#     case 4:
+#         print("Yaxshi")
+#     case 3:
+#         print("Qoniqarli")
+#     case 2:
+#         print("Qoniqarsiz")
+#     case 1:
+#         print("Dabdala")
+#     case _:
+#         print("Bunday baho bebaho")
+#
+# teacher = input("Enter your name: ").lower()
+#
+# match teacher:
+#     case 'ikromjon':                      #'IkromjON' | 'ikromjon' | "IKROMJON":
+#         print("Python Backend teacher!")
+#     case _:
+#         print("Notanish teacher!")
+
+
+
+kun = int(input('Kun raqami: '))
+qoldiq = kun % 7
+
+if 1 <= kun <= 365:
+    match qoldiq:
+        case 1 :#| 8 | 15 | 22:
+            print('Payshanba')
+        case 2: #| 9:
+            print('Juma')
+        case  3:
+            print('Shanba')
+        case 4:
+            print('Yakshanba')
+        case 5:
+            print('Dushanba')
+        case 6:
+            print('Seshanba')
+        case 0:
+            print('Chorshanba')
+else:
+    print('Bunday kun yoq')
---
+++ royxat.py
@@ -0,0 +1,58 @@
+# str    = belgilar (matn)
+# int    = butun sonlar
+# float  = o'nli kasr
+# bool   = True / False
+
+
+# list = o'zgaruvchan, tabtiblangan, takrorlanuvchi ro'yxat
+# tuple = o'zgarmas, tabtiblangan, takrorlanuvchi ro'yxat
+
+# pupil1 = 'Humoyun'
+# pupil2 = 'Muhammadrizo'
+# pupil3 = 'Fathullo'
+# pupil4 = 'Muhammadaziz'
+#             0            1              2             3
+pupils = ['Humoyun', 'Muhammadrizo', 'Fathullo', 'Muhammadaziz']
+# print(pupils[2])
+# print(pupils[-1])
+# print(pupils[-3])
+# print(pupils[-4])
+# print(pupils[0])
+
+# =========== add elements ====================
+pupils.append('Sherzodbek')
+# print(pupils)
+# print(len(pupils), type(pupils) )
+pupils.insert(2, 'Ilyosbek')
+# print(pupils)
+pupils.extend(['Alijon', 'Valijon', 'Karimjon'])
+# print(pupils)
+
+
+# ========== delete elements ==========
+pupils.pop()
+# print(pupils)
+pupils.remove('Ilyosbek')
+# print(pupils)
+pupils.pop(-1)
+print(pupils)
+
+pupils.reverse()
+print(pupils)
+pupils.sort()
+print(pupils)
+
+# print(pupils.count('Alijon'))
+# print(pupils.index('Muhammadrizo'))
+# print(pupils.pop())
+
+
+pupil = ['Alijon', 15, 'Namanagan', False]
+numbers_list = [1, 2, 3, 4, 5]
+numbers_tuple= (1, 2, 3, 4, 5)
+# print(numbers_list)
+# print(max(numbers_list))
+# print(min(numbers_list))
+# print(sum(numbers_list))
+print(type(numbers_list), len(numbers_list), numbers_list)
+print(type(numbers_tuple), len(numbers_tuple), numbers_tuple)
